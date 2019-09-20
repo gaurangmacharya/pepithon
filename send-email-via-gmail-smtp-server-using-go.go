64571fc8862d841ed7dab2a7a757289f07d6b34b
gaurangmacharya <55314851+gaurangmacharya@users.noreply.github.com>
2019-09-20 21:10:31 +0530

    Create send-email-via-gmail-smtp-server-using-go.go

---
+++ send-email-via-gmail-smtp-server-using-go.go
@@ -0,0 +1,44 @@
+package main
+import (
+    "log"
+    "fmt"
+    "bytes"
+    "net/smtp"
+    "mime/quotedprintable"
+)
+
+func main() {
+    from_email := "acharya.gm@gmail.com"
+    password   := "wzsmcipggbsqnjxl"
+    host       := "smtp.gmail.com:587"
+    auth       := smtp.PlainAuth("", from_email, password, "smtp.gmail.com")
+
+    header := make(map[string]string)
+    to_email           := "acharya_gaurang@yahoo.com"
+    header["From"]     = from_email
+    header["To"]       = to_email
+    header["Subject"]  = "Write Your Subject Here"
+
+    header["MIME-Version"]              = "1.0"
+    header["Content-Type"]              = fmt.Sprintf("%s; charset=\"utf-8\"", "text/html")
+    header["Content-Transfer-Encoding"] = "quoted-printable"
+    header["Content-Disposition"]       = "inline"
+
+    header_message := ""
+    for key, value := range header {
+        header_message += fmt.Sprintf("%s: %s\r\n", key, value)
+    }
+
+    body := "<h1>This is your HTML Body</h1>"
+    var body_message bytes.Buffer
+    temp := quotedprintable.NewWriter(&body_message)
+    temp.Write([]byte(body))
+    temp.Close()
+
+    final_message := header_message + "\r\n" + body_message.String()
+    status  := smtp.SendMail(host, auth, from_email, []string{to_email}, []byte(final_message))
+    if status != nil {
+        log.Printf("Error from SMTP Server: %s", status)
+    }
+    log.Print("Email Sent Successfully")
+}
