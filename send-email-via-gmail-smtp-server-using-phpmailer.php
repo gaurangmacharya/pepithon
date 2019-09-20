4b2cbbf11e8ac775f78aa4d31ff98ba88048098a
gaurangmacharya <55314851+gaurangmacharya@users.noreply.github.com>
2019-09-20 19:00:41 +0530

    Create send-email-via-gmail-smtp-server-using-phpmailer.php

---
+++ send-email-via-gmail-smtp-server-using-phpmailer.php
@@ -0,0 +1,34 @@
+<?php
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exception;
+  require 'PHPMailer-master/src/Exception.php';
+  require 'PHPMailer-master/src/PHPMailer.php';
+  require 'PHPMailer-master/src/SMTP.php';
+
+  $mail = new PHPMailer();
+  $mail->IsSMTP();
+
+  $mail->SMTPDebug  = 0;  
+  $mail->SMTPAuth   = TRUE;
+  $mail->SMTPSecure = "tls";
+  $mail->Port       = 587;
+  $mail->Host       = "smtp.gmail.com";
+  $mail->Username   = "your-email@gmail.com";
+  $mail->Password   = "your-gmail-password";
+
+  $mail->IsHTML(true);
+  $mail->AddAddress("recipient-email", "recipient-name");
+  $mail->SetFrom("your-email@gmail.com", "set-from-name");
+  $mail->AddReplyTo("reply-to-email", "reply-to-name");
+  $mail->AddCC("cc-recipient-email", "cc-recipient-name");
+  $mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
+  $content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
+
+  $mail->MsgHTML($content); 
+  if(!$mail->Send()) {
+    echo "Error while sending Email.";
+    var_dump($mail);
+  } else {
+    echo "Email sent successfully";
+  }
+?>
